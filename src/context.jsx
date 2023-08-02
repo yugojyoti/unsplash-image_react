7f64f0e2cd337bb34535f8634bf826cc85b5eefe
yugojyoti <yugojyotimohanta@gmail.com>
2023-08-02 06:22:38 +0530

    REACT_unplash

---
+++ src/context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  return prefersDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode);
+  const [searchTerm, setSearchTerm] = useState("cat");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", isDarkTheme);
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
